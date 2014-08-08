b23fa341ac5f2ada721b76f2a154841b13347ef0
Catherine Tran <tranc489@gmail.com>
2014-08-08 14:09:20 -0700

    make functionality to return name to user.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -5,4 +5,9 @@ describe Task do
     task = Task.new('learn SQL')
     task.should be_an_instance_of Task
   end
+
+  it 'tells you its name' do
+    task = Task.new('learn SQL')
+    task.name.should eq 'learn SQL'
+  end
 end
