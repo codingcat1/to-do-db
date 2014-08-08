df7642c4110d64d3e651010ea761ec51c4b52cd3
Catherine Tran <tranc489@gmail.com>
2014-08-08 15:06:40 -0700

    Add a new database using original template.  Change code to connect to new database.  Add functionality to allow user to save tasks to database.  Make 2 tasks be the same if they have the same name.

--- lib/task.rb
+++ lib/task.rb
@@ -1,3 +1,6 @@
+require 'pg'
+
+
 class Task
 
   def initialize(name)
@@ -8,7 +11,21 @@ class Task
     @name
   end
 
+  def save
+    DB.exec("INSERT INTO tasks (name) VALUES ('#{@name}');")
+  end
+
   def self.all
-    []
+    results = DB .exec("SELECT * FROM tasks;")
+    tasks = []
+    results.each do |result|
+      name = result ['name']
+      tasks << Task.new(name)
+    end
+    tasks
+  end
+
+  def ==(another_task)
+    self.name == another_task.name
   end
 end
